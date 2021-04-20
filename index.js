c5846faa781a0587356d6787c9e0fba7ede0ca9c
Casey Warren <caseywar1017@gmail.com>
2021-04-20 17:06:07 -0400

    shame bot with timeout

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('lets go guillermo');
+});
+
+client.login(process.env.TOKEN);
+
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content.toLowerCase().includes('vampire')) 
+    {
+        const author = message.author.username;
+        console.log(author, 'out of loop')
+
+        let looping = true;
+        client.on('message', message => {
+            console.log(author, 'in loop')
+            if (message.author.bot) return;
+            if (looping && author === message.author.username) 
+            {message.reply('SHAME')}
+        })
+
+        client.setTimeout( () => {
+            looping = false
+            console.log(looping)}, 30000);
+
+    };
+
+    if (message.content.includes('!nandor'))
+    {
+        message.author.id.roles.add('Lightly Shamed')
+        console.log(message.guild.roles.cache)
+    }
+
+});
