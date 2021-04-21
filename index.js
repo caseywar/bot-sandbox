bc97a103bb9b84b49422f4bed98bcf9713b25b8d
Casey Warren <caseywar1017@gmail.com>
2021-04-21 19:51:42 -0400

    creates a webhook on channel command is typed into

--- index.js
+++ index.js
@@ -3,41 +3,60 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('lets go guillermo');
-});
-
 client.login(process.env.TOKEN);
 
 
-client.on('message', message => {
-    if (message.author.bot) return;
-    
-    //if block triggers a 30s timeout where the author will be shamed if they type anything
-    if (message.content.toLowerCase().includes('vampire')) 
-    {
-        const author = message.author.username;
-        console.log(author, 'out of loop')
-
-        let looping = true;
-        client.on('message', message => {
-            console.log(author, 'in loop')
-            if (message.author.bot) return;
-            if (looping && author === message.author.username) 
-            {message.reply('SHAME')}
-        })
-
-        client.setTimeout( () => {
-            looping = false
-            console.log(looping)}, 30000);
-
-    };
-
-    //assigns a role to the message author
-    if (message.content.includes('!nandor'))
-    {
-        message.member.roles.add(message.guild.roles.cache.get('834167900830171156'))
-        console.log(message.guild.roles.cache)
-    }
-
+client.once('ready', () => {
+	console.log('Good to go, boss!');
 });
+
+
+client.on('message', message => {
+	// console.log(message.createdTimestamp);
+	if (message.content.includes('>goshamebot')) {
+        // message.channel.createWebhook("Example Webhook", "https://i.imgur.com/p2qNFag.png")
+        message.channel.createWebhook('Snek', {
+            avatar: 'https://i.imgur.com/mI8XcpG.jpg',
+            reason: 'Needed a cool new Webhook'
+        })
+            .then(console.log)
+            .catch(console.error)
+
+    }
+    
+    if (message.author.bot) return;
+    message.channel.send('shamebo activatio');
+});
+
+
+// client.on('message', message => {
+//     if (message.author.bot) return;
+
+//     //if block triggers a 30s timeout where the author will be shamed if they type anything
+//     if (message.content.toLowerCase().includes('vampire')) 
+//     {
+//         const author = message.author.username;
+//         console.log(author, 'out of loop')
+
+//         let looping = true;
+//         client.on('message', message => {
+//             console.log(author, 'in loop')
+//             if (message.author.bot) return;
+//             if (looping && author === message.author.username) 
+//             {message.reply('SHAME')}
+//         })
+
+//         client.setTimeout( () => {
+//             looping = false
+//             console.log(looping)}, 30000);
+
+//     };
+
+//     //assigns a role to the message author
+//     if (message.content.includes('!nandor'))
+//     {
+//         message.member.roles.add(message.guild.roles.cache.get('834165908913913867'))
+//         console.log(message.guild.roles.cache)
+//     }
+
+// });
